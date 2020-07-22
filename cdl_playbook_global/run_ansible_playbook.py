eb1b78f2bdf81c36f0d186087568520c36e1ede1
Scott Shoaf <scotchoaf@gmail.com>
2020-07-22 15:24:42 -0400

    update skillet yaml file

--- cdl_playbook_global/run_ansible_playbook.py
+++ cdl_playbook_global/run_ansible_playbook.py
@@ -15,42 +15,44 @@
 
 # Authors: Scott Shoaf, Nathan Embery
 
-import click
 import json
 import subprocess
 
+import click
+
+
 @click.command()
 @click.option("-k", "--cdl_preshared_key", help="onboarding preshared_key", type=str, default='')
-@click.option("-ip", "--device_ip", help="device IP address", type=str, default='')
-@click.option("-u", "--username", help="API username", type=str, default='admin')
-@click.option("-p", "--password", help="API password", type=str, default='Paloalto1')
-
+@click.option("-ip", "--TARGET_IP", help="device IP address", type=str, default='')
+@click.option("-u", "--TARGET_USERNAME", help="API username", type=str, default='admin')
+@click.option("-p", "--TARGET_PASSWORD", help="API password", type=str, default='Paloalto1')
 def cli(cdl_preshared_key, device_ip, username, password):
     """
     run ansible playbook for global Cortex Data Lake configuration
     :param cdl_preshared_key: firewall onboarding PreShared_Key for Cortex Data Lake
-    :param device_ip: part of the provider defining device to configure
-    :param username: username to access the device
-    :param password: password to access the device
+    :param TARGET_IP: part of the provider defining device to configure
+    :param TARGET_USERNAME: username to access the device
+    :param TARGET_PASSWORD: password to access the device
     :return: None
     """
 
     # create a simple provider dict to convert to json and pass as ansible extra-var
     provider_dict = {}
     provider_dict['provider'] = {}
-    provider_dict['provider']['ip_address'] = device_ip
-    provider_dict['provider']['username'] = username
-    provider_dict['provider']['password'] = password
+    provider_dict['provider']['ip_address'] = TARGET_IP
+    provider_dict['provider']['username'] = TARGET_USERNAME
+    provider_dict['provider']['password'] = TARGET_PASSWORD
     xvar_provider = json.dumps(provider_dict)
 
     # ansible command line entry with extra vars
-    playbook_cmd = f'ansible-playbook -i inventory.ini cdl.yml'\
-                   f' -e cdl_psk={cdl_preshared_key}'\
+    playbook_cmd = f'ansible-playbook -i inventory.ini cdl.yml' \
+                   f' -e cdl_psk={cdl_preshared_key}' \
                    f' -e "{xvar_provider}"'
 
     # run the playbook and wait until complete
     run_playbook = subprocess.Popen(playbook_cmd, shell=True)
     run_playbook.wait()
+
 
 if __name__ == '__main__':
     cli()
